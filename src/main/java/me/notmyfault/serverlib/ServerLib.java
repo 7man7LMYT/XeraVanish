dad0bef324ea0a846286119072c2e05efbd80d0b
7man7LMYT <67489949+7man7LMYT@users.noreply.github.com>
2021-01-23 18:30:48 -0800

    Update ServerLib.java

--- src/main/java/me/notmyfault/serverlib/ServerLib.java
+++ src/main/java/me/notmyfault/serverlib/ServerLib.java
@@ -1,11 +1,11 @@
-package de.notmyfault.serverlib;
+package me.notmyfault.serverlib;
 
-import static de.notmyfault.serverlib.forks.AirplaneLite.isAirplaneLite;
-import static de.notmyfault.serverlib.forks.AirplaneLiteChunkConcurrency.isAirplaneLiteChunkConcurrency;
-import static de.notmyfault.serverlib.forks.Akarin.isAkarin;
-import static de.notmyfault.serverlib.forks.Forge.isForge;
-import static de.notmyfault.serverlib.forks.KibblePatcher.isKibblePatcher;
-import static de.notmyfault.serverlib.forks.Yatopia.isYatopia;
+import static me.notmyfault.serverlib.forks.AirplaneLite.isAirplaneLite;
+import static me.notmyfault.serverlib.forks.AirplaneLiteChunkConcurrency.isAirplaneLiteChunkConcurrency;
+import static me.notmyfault.serverlib.forks.Akarin.isAkarin;
+import static me.notmyfault.serverlib.forks.Forge.isForge;
+import static me.notmyfault.serverlib.forks.KibblePatcher.isKibblePatcher;
+import static me.notmyfault.serverlib.forks.Yatopia.isYatopia;
 
 public class ServerLib {
 
